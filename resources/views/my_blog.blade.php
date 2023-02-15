f232fcf891bc27ba816bdd4331fc4a98a1dc5e1a
Mark-dev30 <fronterremarco@gmail.com>
2023-02-15 15:33:22 +0100

    bonus completed

--- resources/views/my_blog.blade.php
+++ resources/views/my_blog.blade.php
@@ -34,7 +34,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
-                <h1>HOME</h1>
+                <h1>MY BLOG</h1>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
 
     $hello = 'Hello World';
 
     return view('home', compact('hello'));
-});
+})->name('home');
+
+Route::get('/about-me', function () {
+    return view('about_me');
+})->name('about-me');
+
+Route::get('/testimonials', function () {
+    return view('testimonials');
+})->name('testimonials');
+
+Route::get('/my-blog', function () {
+    return view('my_blog');
+})->name('my-blog');
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
